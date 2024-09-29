02a0f7479c5b17a335e8dcde213bf0d8d3290205
ulipese <felipe.sousa4030@gmail.com>
2024-09-29 20:11:30 -0300

    fix(News): adjust news target

--- src/pages/components/News/NewsMoreInfos.jsx
+++ src/pages/components/News/NewsMoreInfos.jsx
@@ -4,7 +4,7 @@ import { ArrowRight } from "lucide-react";
 export const NewsMoreInfos = ({ link, text, time }) => {
   return (
     <div className="w-full flex flex-row items-center justify-between text-sm">
-      <Link to={link} className="flex flex-row w-[8em] text-eblue font-bold">
+      <Link to={link} target="blank" className="flex flex-row w-[8em] text-eblue font-bold">
         {text} <ArrowRight width="2em" />
       </Link>
       <p className="w-full max-w-[5em] text-right text-gray-300 ">{time}</p>
