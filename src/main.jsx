47b92e3ae9965bec040cf90c3cc44aba6648d859
Ryan Brito <rm554497@fiap.com.br>
2024-10-07 21:17:03 -0300

    Integração com Google Calendário

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,10 @@ import App from './App.jsx'
 import { createClient } from '@supabase/supabase-js'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
-const URL = import.meta.env.VITE_SUPABASE_URL
-const API_KEY = import.meta.env.VITE_CALENDAR_TOKEN
-
 const supabase = createClient(
   // URL e API Key do Projeto
-  URL,
-  API_KEY
+  "https://gazxutqbgrvigglhrevo.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdhenh1dHFiZ3J2aWdnbGhyZXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzMDkxNDcsImV4cCI6MjA0Mzg4NTE0N30.cdyciNEoRRIejlnfG_lAfamOerUsTtmnZoY-xSRPBgM"
 )
 
 createRoot(document.getElementById('root')).render(
